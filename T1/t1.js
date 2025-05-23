5541eff6429571270885e04542d621fa3fbd4dcb
Artur Castro <arturcastro010@gmail.com>
2025-05-23 18:02:52 -0300

    Movimentação de câmera no teclado e mouse + colisão com o chão implementada

--- T1/t1.js
+++ T1/t1.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { PointerLockControls } from '../build/jsm/controls/PointerLockControls.js'
 import MATERIALS from "./components/materials.js";
 import { OrbitControls } from "../build/jsm/controls/OrbitControls.js";
 import { createWall } from "./components/walls.js";
@@ -9,12 +10,16 @@ import { areaGeometrySmall, areaGeometryLarge, blockGeometrySmall, blockGeometry
 import { initRenderer, initCamera, initDefaultBasicLight, onWindowResize } from "../libs/util/util.js";
 import KeyboardState from '../../libs/util/KeyboardState.js'
 
-// Inicialização da cena e utilitárioss
+// Inicialização da cena e utilitários
 const scene = new THREE.Scene();
-const renderer = initRenderer();
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setAnimationLoop( animate );
+  document.body.appendChild( renderer.domElement );
 initDefaultBasicLight(scene);
-let keyboard = new KeyboardState();
 window.addEventListener("resize", () => onWindowResize(camera, renderer), false);
+
 
 // Main camera
 let camPos  = new THREE.Vector3(0, 10, 20);
@@ -54,33 +59,144 @@ addBlockandStairToArea(area2, [30, -3.5, 60], Math.PI);
 addBlockandStairToArea(area3, [0, -3.5, 60], Math.PI);
 addBlockandStairToArea(area4, [0, -3.5, -85], 0);
 
-render();
+//Cria variáveis para movimento
+let controls; //Utilizado para controle da câmera
+let raycaster; //utilizado para colisão
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
 
-function keyboardUpdate() {  
-   // DICA: Insira aqui seu código para mover a câmera
-   keyboard.update(); 
-      if ( keyboard.pressed("A") )     camPos.x -= 1;
-      if ( keyboard.pressed("D") )    camPos.x += 1;
-      if ( keyboard.pressed("SPACE") )       camPos.y += 10;
-      if ( keyboard.pressed("CTRL") )     camPos.y -= 1;
-      if ( keyboard.pressed("S") )   camPos.z += 1;
-      if ( keyboard.pressed("W") ) camPos.z -= 1;
+let prevTime = performance.now(); //timer para sincronizar frames
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
 
-      // if (keyboard.pressed("W")) camera.lookAt(camLook.x += 1);
-      // if (keyboard.pressed("S")) camera.lookAt(camLook.x -= 1);
-      // if (keyboard.pressed("A")) camera.lookAt(camLook.z += 1);
-      // if (keyboard.pressed("D")) camera.lookAt(camLook.z -= 1);
-      // if (keyboard.pressed("Q")) camera.lookAt(camLook.y += 1);
-      // if (keyboard.pressed("E")) camera.lookAt(camLook.y -= 1);
-      camera.position.copy(camPos);
-      //camera.lookAt(camLook);
+//Define funcionamento dos controles
+controls = new PointerLockControls(camera, document.body); //cria nova instância de controle
+window.addEventListener('click', function () {
+  controls.lock(); //ao clicar, trava o mouse para movimentar a câmera  
+});
 
-   //updateCamera();
-}
+scene.add(controls.getObject()); //Adiciona o objeto de controles
 
-// Renderização
-function render() {
-  requestAnimationFrame(render);
-  keyboardUpdate();
-  renderer.render(scene, camera);
-}
+//função acionada quando uma tecla for apertada
+const onKeyDown = function(event) {
+  switch (event.code) {
+    case 'ArrowUp':
+		case 'KeyW':
+			moveForward = true;
+			break;
+
+		case 'ArrowLeft':
+		case 'KeyA':
+			moveLeft = true;
+			break;
+
+		case 'ArrowDown':
+		case 'KeyS':
+			moveBackward = true;
+			break;
+
+		case 'ArrowRight':
+		case 'KeyD':
+			moveRight = true;
+			break;
+
+		case 'Space':
+			if ( canJump === true ) velocity.y += 350;
+			canJump = false;
+			break;
+  }
+};
+
+//função acionada quando uma tecla for solta
+const onKeyUp = function ( event ) {
+
+switch ( event.code ) {
+
+	case 'ArrowUp':
+	case 'KeyW':
+		moveForward = false;
+		break;
+
+	case 'ArrowLeft':
+	case 'KeyA':
+		moveLeft = false;
+		break;
+
+	case 'ArrowDown':
+	case 'KeyS':
+		moveBackward = false;
+		break;
+
+	case 'ArrowRight':
+	case 'KeyD':
+		moveRight = false;
+		break;
+	}
+};
+
+document.addEventListener('keydown', onKeyDown );
+document.addEventListener('keyup', onKeyUp );
+
+raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+const objects = [groundPlane, area1, area2, area3, area4];
+
+//Animação
+function animate() {
+
+				const time = performance.now();
+
+				if ( controls.isLocked === true ) {
+
+					raycaster.ray.origin.copy( controls.getObject().position );
+					raycaster.ray.origin.y -= 10;
+
+					const intersections = raycaster.intersectObjects( objects, false );
+
+					const onObject = intersections.length > 0;
+
+					const delta = ( time - prevTime ) / 1000;
+
+					velocity.x -= velocity.x * 10.0 * delta;
+					velocity.z -= velocity.z * 10.0 * delta;
+
+					velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+					direction.z = Number( moveForward ) - Number( moveBackward );
+					direction.x = Number( moveRight ) - Number( moveLeft );
+					direction.normalize(); // this ensures consistent movements in all directions
+
+					if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+					if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+					if ( onObject === true ) {
+
+						velocity.y = Math.max( 0, velocity.y );
+						canJump = true;
+
+					}
+
+					controls.moveRight( - velocity.x * delta );
+					controls.moveForward( - velocity.z * delta );
+
+					controls.getObject().position.y += ( velocity.y * delta ); // new behavior
+
+					if ( controls.getObject().position.y < 10 ) {
+
+						velocity.y = 0;
+						controls.getObject().position.y = 10;
+
+						canJump = true;
+
+					}
+
+				}
+
+				prevTime = time;
+
+				renderer.render( scene, camera );
+
+			}
