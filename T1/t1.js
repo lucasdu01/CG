93e3a0173bb5d099bfbaf45d7a931cdf735aa518
LucasRochaTR <lucas.lgr.01@gmail.com>
2025-05-23 16:53:34 -0300

    adicionando camera

--- T1/t1.js
+++ T1/t1.js
@@ -7,15 +7,24 @@ import { createGroundPlaneXZ } from "./components/groundPlane.js";
 import { addBlockandStairToArea, addBlocksToArea, addStairToArea } from "./util.js";
 import { areaGeometrySmall, areaGeometryLarge, blockGeometrySmall, blockGeometryLarge, wallGeometry, groundPlaneGeometry, block1area1Geometry } from "./components/geometries.js";
 import { initRenderer, initCamera, initDefaultBasicLight, onWindowResize } from "../libs/util/util.js";
+import KeyboardState from '../../libs/util/KeyboardState.js'
 
 // Inicialização da cena e utilitárioss
 const scene = new THREE.Scene();
 const renderer = initRenderer();
-const camera = initCamera(new THREE.Vector3(0, 500, 0));
 initDefaultBasicLight(scene);
-const orbit = new OrbitControls(camera, renderer.domElement);
-
+let keyboard = new KeyboardState();
 window.addEventListener("resize", () => onWindowResize(camera, renderer), false);
+
+// Main camera
+let camPos  = new THREE.Vector3(0, 10, 20);
+let camUp   = new THREE.Vector3(0.0, 1.0, 0.0);
+let camLook = new THREE.Vector3(0.0, 10.0, 20.0);
+
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+   camera.position.copy(camPos);
+   camera.up.copy( camUp );
+   camera.lookAt(camLook);
 
 // Eixos e grid
 scene.add(new THREE.AxesHelper(12));
@@ -40,21 +49,38 @@ const area4 = createArea(areaGeometryLarge, MATERIALS.green, [0, 4, 150]);
 scene.add(area1, area2, area3, area4);
 
 // Adiciona escadas
-addStairToArea(area1, [0, -3.5, 60], Math.PI);
-addStairToArea(area2, [0, -3.5, 60], Math.PI);
-addStairToArea(area3, [0, -3.5, 60], Math.PI);
-addStairToArea(area4, [0, -3.5, -85], 0);
-
-// Adiciona blocos
-addBlocksToArea(area1, blockGeometrySmall, MATERIALS.lightgreen, [[-30, 0, 55.5], [30, 0, 55.5]]);
-addBlocksToArea(area2, blockGeometrySmall, MATERIALS.red, [[-30, 0, 55.5], [30, 0, 55.5]]);
-addBlocksToArea(area3, blockGeometrySmall, MATERIALS.blue, [[-30, 0, 55.5], [30, 0, 55.5]]);
-addBlocksToArea(area4, blockGeometryLarge, MATERIALS.green, [[-67.5, 0, -80.5], [67.5, 0, -80.5]]);
+addBlockandStairToArea(area1, [-30, -3.5, 60], Math.PI);
+addBlockandStairToArea(area2, [30, -3.5, 60], Math.PI);
+addBlockandStairToArea(area3, [0, -3.5, 60], Math.PI);
+addBlockandStairToArea(area4, [0, -3.5, -85], 0);
 
 render();
+
+function keyboardUpdate() {  
+   // DICA: Insira aqui seu código para mover a câmera
+   keyboard.update(); 
+      if ( keyboard.pressed("A") )     camPos.x -= 1;
+      if ( keyboard.pressed("D") )    camPos.x += 1;
+      if ( keyboard.pressed("SPACE") )       camPos.y += 10;
+      if ( keyboard.pressed("CTRL") )     camPos.y -= 1;
+      if ( keyboard.pressed("S") )   camPos.z += 1;
+      if ( keyboard.pressed("W") ) camPos.z -= 1;
+
+      // if (keyboard.pressed("W")) camera.lookAt(camLook.x += 1);
+      // if (keyboard.pressed("S")) camera.lookAt(camLook.x -= 1);
+      // if (keyboard.pressed("A")) camera.lookAt(camLook.z += 1);
+      // if (keyboard.pressed("D")) camera.lookAt(camLook.z -= 1);
+      // if (keyboard.pressed("Q")) camera.lookAt(camLook.y += 1);
+      // if (keyboard.pressed("E")) camera.lookAt(camLook.y -= 1);
+      camera.position.copy(camPos);
+      //camera.lookAt(camLook);
+
+   //updateCamera();
+}
 
 // Renderização
 function render() {
   requestAnimationFrame(render);
+  keyboardUpdate();
   renderer.render(scene, camera);
 }
