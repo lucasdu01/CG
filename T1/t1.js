b670899d8688f2a3c4534678c39adc52ecb0333a
LucasRochaTR <lucas.lgr.01@gmail.com>
2025-05-22 22:01:58 -0300

    ajustando posicao areas

--- T1/t1.js
+++ T1/t1.js
@@ -63,10 +63,10 @@ let area3 = new THREE.Mesh(areaGeometry, blue); // area 3
 let areaGeometryLarge =new THREE.BoxGeometry(250, 8, 150);
 let area4 = new THREE.Mesh(areaGeometryLarge, purple);
 // position the areas
-area1.position.set(-150.0, 4.0, 150.0);
-area2.position.set(0.0, 4.0, 150.0);
-area3.position.set(150.0, 4.0, 150.0);
-area4.position.set(0.0, 4.0, -150.0);
+area1.position.set(-150.0, 4.0, -150.0);
+area2.position.set(0.0, 4.0, -150.0);
+area3.position.set(150.0, 4.0, -150.0);
+area4.position.set(0.0, 4.0, 150.0);
 // add the areas to the scene
 scene.add(area1);
 scene.add(area2);
@@ -74,21 +74,23 @@ scene.add(area3);
 scene.add(area4);
 
 //create stairs
-const staircase1 = createStaircaseWithCollision({color: '#FFFF00'});
-const staircase2 = createStaircaseWithCollision({color: '#FFFF00'});
-const staircase3 = createStaircaseWithCollision({color: '#FFFF00'});
-const staircase4 = createStaircaseWithCollision({color: '#FFFF00'});
+const staircase1 = createStaircaseWithCollision({color: '#FFFFF'});
+const staircase2 = createStaircaseWithCollision({color: '#FFFFF'});
+const staircase3 = createStaircaseWithCollision({color: '#FFFFF'});
+const staircase4 = createStaircaseWithCollision({color: '#FFFFF'});
 
 //add staircases to the areas
 area1.add(staircase1);
-staircase1.position.set(0,-3.5,-60);
+staircase1.position.set(0,-3.5,60);
+staircase1.rotation.y = Math.PI;
 area2.add(staircase2);
-staircase2.position.set(0,-3.5,-60);
+staircase2.position.set(0,-3.5,60);
+staircase2.rotation.y = Math.PI;
 area3.add(staircase3);
-staircase3.position.set(0,-3.5,-60);
+staircase3.position.set(0,-3.5,60);
+staircase3.rotation.y = Math.PI;
 area4.add(staircase4);
-staircase4.position.set(0,-3.5,85);
-staircase4.rotateY(Math.PI);
+staircase4.position.set(0,-3.5,-85);
 
 // Blocos ao lado da escada das áreas 1 a 3
 let blockGeometry = new THREE.BoxGeometry(40, 8, 11.5);
@@ -98,8 +100,8 @@ let blockRight1 = new THREE.Mesh(blockGeometry, blockMaterial);
 
 
 // Posiciona os blocos
-blockLeft1.position.set(-30, 0, -55.5);
-blockRight1.position.set(30, 0, -55.5);
+blockLeft1.position.set(-30, 0, 55.5);
+blockRight1.position.set(30, 0, 55.5);
 
 // Adiciona os blocos à área
 area1.add(blockLeft1);
@@ -118,8 +120,8 @@ let blockLeft2 = new THREE.Mesh(blockGeometry2, blockMaterial2);
 let blockRight2 = new THREE.Mesh(blockGeometry2, blockMaterial2);
 
 // Posiciona os blocos
-blockLeft2.position.set(-67.5, 0, 80.5);
-blockRight2.position.set(67.5, 0, 80.5);
+blockLeft2.position.set(-67.5, 0, -80.5);
+blockRight2.position.set(67.5, 0, -80.5);
 
 // Adiciona os blocos à área
 area4.add(blockLeft2);
